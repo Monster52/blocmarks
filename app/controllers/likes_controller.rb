c3e7c36f314c05b95b2be82c76aa291ce3599498
Ross Waguespack <rosswaguespack@gmail.com>
2016-06-27 16:47:00 -0500

    fixing likes / unlike not working

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
   def create
     @bookmark = Bookmark.find(params[:bookmark_id])
     @like = current_user.likes.build(bookmark: @bookmark)
-  
+
     if @like.save
       flash[:notice] = "Bookmark liked."
       redirect_to [@bookmark.topic, @bookmark]
@@ -18,7 +18,7 @@ class LikesController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:bookmark_id])
     @like = current_user.likes.build(bookmark: @bookmark)
-  
+
     if @like.destroy
       flash[:notice] = "\"#{@bookmark.url}\" unliked"
       redirect_to [@bookmark.topic, @bookmark]
