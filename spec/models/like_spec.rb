3f625727904076fa5d80653c806c763d36cdebe3
Ross Waguespack <rosswaguespack@gmail.com>
2016-06-28 23:32:56 -0500

    Add likes test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "associations" do
+    it { should belong_to(:bookmark) }
+    it { should belong_to(:user) }
+  end
+
 end
